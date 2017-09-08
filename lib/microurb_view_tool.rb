5a719f8d4495d0199b9ca9fb01ccee4845bee88a
danale <danale@danales-MacBook-Pro.local>
2017-09-08 08:03:35 -0400

    Dev tool initial commit

---
+++ lib/microurb_view_tool.rb
@@ -0,0 +1,6 @@
+require "microurb_view_tool/version"
+require "microurb_view_tool/renderer"
+
+module MicrourbViewTool
+  # Your code goes here...
+end
---
+++ lib/microurb_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MicrourbViewTool
+  VERSION = "0.1.0"
+end
